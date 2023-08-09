49d7befabae9215b48f4b47c8f75399990601b94
Serhii Tsyntsar <serhii.tsyntsar@TX-N-1094.local>
2023-08-09 16:52:56 +0300

    Cerrunt user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,19 @@
 import { StatusCodes } from 'http-status-codes';
-// import User from '../models/UserModel.js';
+import User from '../models/UserModel.js';
 // import Job from '../models/JobModel.js';
 
 export const getCurrentUser = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'Get current user' });
-};
+  const user = await User.findOne({ _id: req.user.userId });
+  // Delete a password from the user object
+  const userWithoutPassword = user.toJSON();
 
-export const getApplicationStats = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: 'Application stats' });
+  res.status(StatusCodes.OK).json({ user: userWithoutPassword });
 };
 
 export const updateUser = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: 'Update user' });
+};
+
+export const getApplicationStats = async (req, res) => {
+  res.status(StatusCodes.OK).json({ msg: 'Application stats' });
 };
--- models/UserModel.js
+++ models/UserModel.js
@@ -19,4 +19,11 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
+// Delete a password from the user object
+UserSchema.methods.toJSON = function () {
+  var obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
 export default mongoose.model('User', UserSchema);

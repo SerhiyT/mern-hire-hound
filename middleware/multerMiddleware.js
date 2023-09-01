3d8dae390f1ef46ff10f8e3748da45ecd4b81df6
Serhii Tsyntsar <serhii.tsyntsar@TX-N-1094.local>
2023-09-01 16:55:43 +0300

    switch diskStorage to memoryStorage for image uploads (multer & dataParser

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,17 +1,16 @@
 import multer from 'multer';
+import DataParser from 'datauri/parser.js';
+import path from 'path';
 
-const storage = multer.diskStorage({
-  destination: (req, file, callBack) => {
-    // set the directory where uploaded files will be stored
-    callBack(null, 'public/uploads');
-  },
-  filename: (req, file, callBack) => {
-    const fileName = file.originalname;
-    // set the name of the uploaded file
-    callBack(null, fileName);
-  },
-});
+const storage = multer.memoryStorage();
 const upload = multer({ storage });
+
+const parser = new DataParser();
+
+export const formatImage = (file) => {
+  const fileExtension = path.extname(file.originalname).toString();
+  return parser.format(fileExtension, file.buffer).content;
+};
 
 export default upload;
 
